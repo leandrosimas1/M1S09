6a080410c7817f91d51a67be28260900b6f8762f
leandrosimas1 <lesimas2001@gmail.com>
2025-05-17 00:26:06 -0300

    first commit

---
+++ src/main/java/br/senai/futurodev/exercicio/M1S09/M1S09Application.java
@@ -0,0 +1,13 @@
+package br.senai.futurodev.exercicio.M1S09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1S09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1S09Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/senai/futurodev/exercicio/M1S09/M1S09ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.futurodev.exercicio.M1S09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1S09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
